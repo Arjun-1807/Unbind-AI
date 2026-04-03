9ccb9db51acd2a2526cacb9d0b2d6a8bc214dd5f
Sachin-0001 <sachin.samprit@gmail.com>
2026-04-03 15:48:26 +0530

    preparing for deployement<frontend>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+const BACKEND_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://unbind-backend.vercel.app";
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${BACKEND_URL}/api/:path*`,
       },
     ];
   },
